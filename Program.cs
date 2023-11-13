7862ca3ac03295016549c13b49349ed4645d917b
MariaDimova <150257957+MariaDimova@users.noreply.github.com>
2023-11-13 19:30:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+class HelloCSharp
+{
+static void Main()
+{
+System.Console.WriteLine("Hello, C#!");
+}
+}
